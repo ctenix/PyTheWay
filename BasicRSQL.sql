ee8209c99210f5df8b29f8656fed77e79f33f63f
Tenix Chang <tenix@outlook.com>
2017-12-27 15:08:07 +0800

    add BasicRSQL

---
+++ BasicRSQL.sql
@@ -0,0 +1,22 @@
+-- sp_configure 'external scripts enabled', 1
+-- reconfigure
+-- Test whether R service is enabled
+EXEC sp_execute_external_script
+  @language =N'R',
+  @script=N'OutputDataSet<-InputDataSet',
+  @input_data_1 =N'SELECT 1 AS hello'
+  WITH RESULT SETS (([hello] int not null));
+GO
+-- Create some simple test data
+CREATE TABLE RTestData ([col1] int not null) ON [PRIMARY]
+INSERT INTO RTestData   VALUES (1);
+INSERT INTO RTestData   VALUES (10);
+INSERT INTO RTestData   VALUES (100) ;
+GO
+-- Get the same data using R script
+EXECUTE sp_execute_external_script
+      @language = N'R'
+    , @script = N' OutputDataSet <- InputDataSet;'
+    , @input_data_1 = N' SELECT *  FROM RTestData;'
+    WITH RESULT SETS (([NewColName] int NOT NULL));
+    GO
