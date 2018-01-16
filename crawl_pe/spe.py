1cfff598954df54f7e63831bc868149c384007cb
tenix <tenix@outlook.com>
2018-01-16 23:54:08 +0800

    add toString function

--- crawl_pe/spe.py
+++ crawl_pe/spe.py
@@ -2,19 +2,21 @@
 from common import get_page
 from common import tree
 from common import save_list
+from lxml import html
+
 url = "http://www.spe.org/awards/regional-distinguished-achievement-award-petroleum-engineering-faculty.php"
 page = get_page(url)
 tree = tree(page)
 
-scholars = tree.cssselect('div.details.toggle-content.hidden')
+scholars = tree.cssselect('div.details.toggle-content.hidden > p')
 n = len(scholars)
 info = []
 for scholar in scholars:
-    info.append(scholar.text_content())
+    info.append(html.tostring(scholar))
 #print(info)
 #print("\n")
 print(info[0])
-#save_list(info)
+save_list(info)
 
 
 
