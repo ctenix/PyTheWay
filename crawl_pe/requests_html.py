f873fc64999fc2e1f24a3434eb3e73532293ece2
Tenix Chang <tenix@outlook.com>
2018-01-16 17:03:52 +0800

    add spe

--- crawl_pe/requests_html.py
+++ crawl_pe/requests_html.py
@@ -1,6 +1,6 @@
 import requests
 from lxml import html
-page = requests.get('https://www.aiche.org/community/awards/winners/28951')
+page = requests.get('http://www.spe.org/awards/regional-distinguished-achievement-award-petroleum-engineering-faculty.php')
 tree = html.fromstring(page.content)
 print(type(tree))
 
